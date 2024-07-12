3f2f4a85bff0d80320aaf4fd017dafaa38a9b5be
KhanBazeela <bazeelakhan9@gmail.com>
2024-07-12 23:52:04 +0530

    new

--- Assignment basic/gg.cs
+++ Assignment basic/gg.cs
@@ -1,6 +1,10 @@
-string name = "John Doe";
-string name2 = "Jane Doe";
-string fullName = name + " " + name2;
-Console.WriteLine(fullName);    
-name = string.empty;
+// string name = "John Doe";
+// string name2 = "Jane Doe";
+// string fullName = name + " " + name2;
+// Console.WriteLine(fullName);    
+// name = string.empty;
     
+
+ Console.WriteLine("Think of a number:");
+int myNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your number is: " + myNum);
